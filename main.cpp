d28e089127a2e28275bfac762117629147b1e01a
Yidafiren <alexsong146@gmail.com>
2026-03-04 14:16:12 -0700

    Assignment 5 flipped fix

--- main.cpp
+++ main.cpp
@@ -38,11 +38,17 @@ Pixel parse_line(const string &line)
     return p;
 }
 
-void average_colors(vector<Pixel> &pixel_list)
+void average_colors(const vector<Pixel> &pixel_list)
 {
-    double sum_r = 0.0;
-    double sum_g = 0.0;
-    double sum_b = 0.0;
+    if (pixel_list.size() == 0)
+    {
+        cout << "No pixels loaded.\n";
+        return;
+    }
+
+    double sum_r = 0;
+    double sum_g = 0;
+    double sum_b = 0;
 
     for (size_t i = 0; i < pixel_list.size(); i++)
     {
@@ -51,33 +57,60 @@ void average_colors(vector<Pixel> &pixel_list)
         sum_b += pixel_list[i].b;
     }
 
-    double n = (double)pixel_list.size();
-    if (n == 0)
-    {
-        cout << "No pixels loaded.\n";
-        return;
-    }
+    double n = pixel_list.size();
 
-    cout << "average r = " << (sum_r / n) << "\n";
-    cout << "average g = " << (sum_g / n) << "\n";
-    cout << "average b = " << (sum_b / n) << "\n";
+    cout << "average r = " << sum_r / n << endl;
+    cout << "average g = " << sum_g / n << endl;
+    cout << "average b = " << sum_b / n << endl;
 }
 
 void flip_vertically(vector<Pixel> &pixel_list)
 {
+    int max_x = 0;
     int max_y = 0;
-    for (size_t i = 0; i < pixel_list.size(); i++)
-    {
-        if (pixel_list[i].y > max_y)
-        {
-            max_y = pixel_list[i].y;
-        }
-    }
 
     for (size_t i = 0; i < pixel_list.size(); i++)
     {
-        pixel_list[i].y = max_y - pixel_list[i].y;
+        if (pixel_list[i].x > max_x)
+            max_x = pixel_list[i].x;
+
+        if (pixel_list[i].y > max_y)
+            max_y = pixel_list[i].y;
     }
+
+    vector<vector<Pixel>> grid(max_y + 1, vector<Pixel>(max_x + 1));
+    vector<vector<bool>> exists(max_y + 1, vector<bool>(max_x + 1, false));
+
+    for (size_t i = 0; i < pixel_list.size(); i++)
+    {
+        int x = pixel_list[i].x;
+        int y = pixel_list[i].y;
+
+        grid[y][x] = pixel_list[i];
+        exists[y][x] = true;
+    }
+
+    vector<Pixel> result;
+
+    for (size_t i = 0; i < pixel_list.size(); i++)
+    {
+        Pixel p = pixel_list[i];
+
+        int x = p.x;
+        int y = p.y;
+        int mirror_y = max_y - y;
+
+        if (exists[mirror_y][x])
+        {
+            p.r = grid[mirror_y][x].r;
+            p.g = grid[mirror_y][x].g;
+            p.b = grid[mirror_y][x].b;
+        }
+
+        result.push_back(p);
+    }
+
+    pixel_list = result;
 }
 
 int main(int argc, char *argv[])
@@ -93,19 +126,17 @@ int main(int argc, char *argv[])
 
     if (!fin.is_open())
     {
-        cout << "Could not open file: " << filename << "\n";
+        cout << "Could not open file\n";
         return 1;
     }
 
     vector<Pixel> pixel_list;
-
     string line;
+
     while (getline(fin, line))
     {
         if (line.size() == 0)
-        {
             continue;
-        }
 
         Pixel p = parse_line(line);
         pixel_list.push_back(p);
@@ -113,15 +144,16 @@ int main(int argc, char *argv[])
 
     fin.close();
 
-    cout << "pixels loaded = " << pixel_list.size() << "\n";
+    cout << "pixels loaded = " << pixel_list.size() << endl;
 
     average_colors(pixel_list);
     flip_vertically(pixel_list);
 
     ofstream fout("flipped.dat");
+
     if (!fout.is_open())
     {
-        cout << "Could not create flipped.dat\n";
+        cout << "Could not create output file\n";
         return 1;
     }
 
@@ -137,5 +169,6 @@ int main(int argc, char *argv[])
     fout.close();
 
     cout << "Wrote flipped.dat\n";
+
     return 0;
 }
